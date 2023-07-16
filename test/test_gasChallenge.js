b09b819551659c7afb4c2477043f5026b9b88b62
Troj Anvood <trojanvood@gmail.com>
2023-07-16 08:19:15 -0700

    Gas challenge bounty completed.

---
+++ test/test_gasChallenge.js
@@ -0,0 +1,28 @@
+const { ethers } = require("hardhat");
+const { expect } = require("chai");
+
+describe("Deploy Gas Challenge Contract", () => {
+  let gas_contract;
+
+  beforeEach(async () => {
+    const gas_challenge_contract = await ethers.getContractFactory(
+      "gasChallenge"
+    );
+    gas_contract = await gas_challenge_contract.deploy();
+  });
+
+  describe("Compute Gas", () => {
+    it("Should return lower gas", async () => {
+      await gas_contract.notOptimizedFunction();
+      await gas_contract.optimizedFunction();
+    });
+  });
+
+  describe("Check Sum Of Array", () => {
+    it("Should return 0", async () => {
+      await gas_contract.optimizedFunction();
+      const sum = await gas_contract.getSumOfArray();
+      expect(sum).to.equal(0);
+    });
+  });
+});
